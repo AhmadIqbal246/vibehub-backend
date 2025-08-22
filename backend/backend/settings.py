682e9493fe1cce389f45ecc4c0516792ecf5935e
AhmadIqbal246 <160164019+AhmadIqbal246@users.noreply.github.com>
2025-08-22 23:19:03 +0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://vibe-hub-eight.vercel.app',
 ]
 
 # Add Railway domains if provided
